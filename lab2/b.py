02015447d25335e8718439b1a4687ef419af3f7f
Viktor Kindrat <kindratvictor07@gmail.com>
2023-09-19 19:46:25 +0300

    lab 2. lab 1 venv recreation

---
+++ lab2/b.py
@@ -0,0 +1,17 @@
+# Range: [0, 1]
+# Step: 0.1
+# Divergence: 0.0001
+
+k = 0
+x = 0
+
+operations_result = 0
+
+while 0 <= x <= 1:
+    operations_result += (((- 1) ** k) * (x ** (2 * k + 3))) / ((2 * k + 1) * (2 * k + 3))
+
+    x += round(x + 0.1, 3)
+    k += 1
+
+print("summa is:", round(operations_result, 4))
+
